4a3d4c0763e8ecb3098bb3fa22bdbbb7e4b07c37
Razvan Rotaru <razvan.rotaru@tractorsetgo.com>
2022-04-03 11:12:46 +0300

    add input field

--- lib/data/simplify_api.dart
+++ lib/data/simplify_api.dart
@@ -4,25 +4,41 @@ import 'package:http/http.dart';
 import 'package:movie_db/models/index.dart';
 
 class SimplifyApi {
-  SimplifyApi({this.url = 'localhost:8000'}) : client = Client();
+  SimplifyApi({this.url = '//TODO'}) : client = Client();
 
   final String url;
   final Client client;
 
   Future<List<Sentence>> getSentences({required String complexSentence}) async {
-    final Map<String, String> params = {
-      'sentence': complexSentence,
-    };
-    final Uri uri = Uri.http(url, '/simplify', params);
-    final Response resp = await client.get(uri);
+    // final Uri uri = Uri.parse('$url/simplify?sentence=$complexSentence');
+    // final Response resp = await client.get(uri);
+    //
+    // if (resp.statusCode >= 400) {
+    //   throw StateError('Unsuccessful connection: ${resp.body}');
+    // }
 
-    if (resp.statusCode >= 400) {
-      throw StateError('Unsuccessful connection: ${resp.body}');
+    const String cache = '''
+    {
+      "sentences": 
+      [
+        {
+          "id": 0,
+          "text": "my bad",
+          "verb": "",
+          "subject": "bad"
+        },
+        {
+          "id": 1,
+          "text": "it's raining again",
+          "verb": "raining",
+          "subject": "it"
+        }
+      ]
     }
+    ''';
 
     final Map<dynamic, dynamic> body =
-        jsonDecode(resp.body) as Map<dynamic, dynamic>;
-    // final Map<dynamic, dynamic> data = body['data'] as Map<dynamic, dynamic>;
+    jsonDecode(/*resp.body*/ cache) as Map<dynamic, dynamic>;
     final List<dynamic> sentences = body['sentences'] as List<dynamic>;
 
     return sentences //
--- lib/presentation/chat_page.dart
+++ lib/presentation/chat_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:movie_db/actions/index.dart';
 import 'package:movie_db/container/loading_container.dart';
 import 'package:movie_db/presentation/message_widget.dart';
 import 'package:redux/redux.dart';
@@ -14,6 +18,23 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void _sendMessage(Store<AppState> store, String message) {
+    final String json = '''
+    {
+      "id": ${Random().nextInt(10000)},
+      "text": "$message",
+      "sender": "user"
+    }
+    ''';
+
+    store.dispatch(SendMessageAction(message: Message.fromJson(jsonDecode(json))));
+    setState(() {
+      _textEditingController.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Store<AppState> store = StoreProvider.of<AppState>(context);
@@ -22,16 +43,50 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         title: const Center(child: Text('Chat RM')),
       ),
-      body: LoadingContainer(
-        builder: (BuildContext context, bool isLoading) {
-          if (isLoading) return const CircularProgressIndicator.adaptive();
-          return ListView.builder(
-            itemCount: store.state.messages.length,
-            itemBuilder: (BuildContext context, int index) {
-              return MessageWidget(message: store.state.messages[index]);
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          LoadingContainer(
+            builder: (BuildContext context, bool isLoading) {
+              if (isLoading) {
+                return const CircularProgressIndicator.adaptive();
+              }
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: store.state.messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return MessageWidget(message: store.state.messages[index]);
+                },
+              );
             },
-          );
-        },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    textInputAction: TextInputAction.go,
+                    controller: _textEditingController,
+                    decoration: const InputDecoration(
+                      constraints: BoxConstraints(
+                        maxHeight: 150,
+                      ),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your message here...',
+                    ),
+                    onFieldSubmitted: (String? text) => text ?? _sendMessage(store, text!),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => _sendMessage(store, _textEditingController.text),
+                  icon: const Icon(Icons.send),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
