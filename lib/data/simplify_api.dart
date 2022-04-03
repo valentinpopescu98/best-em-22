0c2664b291611764a1ee33716da249dda47ff190
Razvan Rotaru <razvan.rotaru@tractorsetgo.com>
2022-04-03 11:21:55 +0300

    revert simplify_api to working status

--- lib/data/simplify_api.dart
+++ lib/data/simplify_api.dart
@@ -4,41 +4,25 @@ import 'package:http/http.dart';
 import 'package:movie_db/models/index.dart';
 
 class SimplifyApi {
-  SimplifyApi({this.url = '//TODO'}) : client = Client();
+  SimplifyApi({this.url = 'localhost:8000'}) : client = Client();
 
   final String url;
   final Client client;
 
   Future<List<Sentence>> getSentences({required String complexSentence}) async {
-    // final Uri uri = Uri.parse('$url/simplify?sentence=$complexSentence');
-    // final Response resp = await client.get(uri);
-    //
-    // if (resp.statusCode >= 400) {
-    //   throw StateError('Unsuccessful connection: ${resp.body}');
-    // }
+    final Map<String, String> params = {
+      'sentence': complexSentence,
+    };
+    final Uri uri = Uri.http(url, '/simplify', params);
+    final Response resp = await client.get(uri);
 
-    const String cache = '''
-    {
-      "sentences": 
-      [
-        {
-          "id": 0,
-          "text": "my bad",
-          "verb": "",
-          "subject": "bad"
-        },
-        {
-          "id": 1,
-          "text": "it's raining again",
-          "verb": "raining",
-          "subject": "it"
-        }
-      ]
+    if (resp.statusCode >= 400) {
+      throw StateError('Unsuccessful connection: ${resp.body}');
     }
-    ''';
 
     final Map<dynamic, dynamic> body =
-    jsonDecode(/*resp.body*/ cache) as Map<dynamic, dynamic>;
+    jsonDecode(resp.body) as Map<dynamic, dynamic>;
+    // final Map<dynamic, dynamic> data = body['data'] as Map<dynamic, dynamic>;
     final List<dynamic> sentences = body['sentences'] as List<dynamic>;
 
     return sentences //
